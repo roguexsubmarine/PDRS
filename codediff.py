4932684f9c9224ef36edfc8da7f0bb1ecabe8ff8
aaryant22 <144805557+aaryant22@users.noreply.github.com>
2024-04-13 14:58:53 +0530

    empty commit

--- codediff.py
+++ codediff.py
@@ -1,9 +1,7 @@
 import plagiarism
 import os
 
-ext = ('.py', '.c', '.cpp', '.txt', '.java', '.html')
-
-
+ext = ('.py', '.c', '.cpp', '.txt','.java','.html')
 def strip_lines_and_return(p):
     stripped_lines = []
 
@@ -12,7 +10,7 @@ def strip_lines_and_return(p):
         doc = docx.Document(p)
         for paragraph in doc.paragraphs:
             stripped_lines.append(paragraph.text.strip())
-
+            
     elif p.endswith('.pdf'):
         import PyPDF2
         pdfFileObject = open(p, 'rb')
@@ -27,7 +25,7 @@ def strip_lines_and_return(p):
             if cell.cell_type == 'code':
                 source_code_lines = cell.source.split('\n')
                 stripped_lines.extend(source_code_lines)
-
+        
     elif p.endswith(ext):
         with open(p, encoding='utf-8') as f:
             content = f.readlines()
